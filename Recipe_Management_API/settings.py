43caa1e0473c7a3a833213736f5bd1698245c514
jackobhat911@gmail.com <jackobhat911@gmail.com>
2024-10-01 18:19:01 +0300

    updating settings

--- Recipe_Management_API/settings.py
+++ Recipe_Management_API/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q#(tkfby!!d-2ztcql1-k9lflx&a*an@0pk4p)kl55hw1)^wv('
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -103,17 +105,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
     }
-# else: 
 
-#     DATABASES = {
-        
-#         'default': dj_database_url.config(
-#             # Replace this value with your local database's connection string.
-#             default='postgresql://recipe_management_postgresql_user:1u1CB890tOXE9PuDDOAaI8zZAsbMgMdC@dpg-crtsmqm8ii6s73alo8g0-a/recipe_management_postgresql',
-#             conn_max_age=600
-#         )
-#     }
 
+DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
