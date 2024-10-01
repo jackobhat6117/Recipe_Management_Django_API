142635740c4ea3a4d1deaa256696fe49cd8e42d0
jackobhat6117 <94125198+jackobhat6117@users.noreply.github.com>
2024-10-01 17:05:29 +0300

    Update wsgi.py

--- Recipe_Management_API/wsgi.py
+++ Recipe_Management_API/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recipe_Management_Django_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recipe_Management_API.settings')
 
 application = get_wsgi_application()
