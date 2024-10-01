0ce104a6e50a5b5f0e38b4135ae67fe15eff6f80
jackobhat911@gmail.com <jackobhat911@gmail.com>
2024-10-01 17:01:54 +0300

    updating the error

--- Recipe_Management_API/settings.py
+++ Recipe_Management_API/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q#(tkfby!!d-2ztcql1-k9lflx&a*an@0pk4p)kl55hw1)^wv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
